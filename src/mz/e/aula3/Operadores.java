82729a6c14aa1dd7accc1a22c6156cfb89b33b03
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-04 19:16:13 +0200

    Introdução ao estudo de variáveis

---
+++ src/mz/e/aula3/Operadores.java
@@ -0,0 +1,18 @@
+
+package mz.e.aula3;
+
+/**
+ *
+ * @author joseseie
+ * @author Filipe Mangue
+ * @author José Romão
+ * @author Aly Macome
+ * @author Abdul Rasac
+ * @author Adriano Uaeca
+ * @author Valter Masssango
+ * @author Efraime Mutole
+ * 
+ */
+public class Operadores {
+    
+}
---
+++ src/mz/e/aula3/Variaveis.java
@@ -0,0 +1,37 @@
+
+package mz.e.aula3;
+
+/**
+ *
+ * @author joseseie
+ * @author Filipe Mangue
+ * @author José Romão
+ * @author Aly Macome
+ * @author Abdul Rasac
+ * @author Adriano Uaeca
+ * @author Valter Masssango
+ * @author Efraime Mutole
+ * 
+ */
+public class Variaveis {
+    
+    public static void main (String[] args) {
+              
+        // Variaveis inteiras
+        int idade = 10;
+        int tamanho = 9;
+        
+        // Variaveis double ou decimais
+        double preco = 10.5;
+        double temperatura;
+        
+        // Variaveis que contém apenas um caracter
+        char sexo = '9';
+        
+        
+        // Variaveis para guardar uma cadeira de caracteres
+        String sexoStr = "Masculino";
+        
+    }
+    
+}
