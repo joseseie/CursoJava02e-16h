0b55a542be13ab49d2768840143d4ff9dbff5a34
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-04 18:27:51 +0200

    Segunda correção adicionada

--- src/mz/e/aulas/OlaMundo.java
+++ src/mz/e/aulas/OlaMundo.java
@@ -1,12 +1,16 @@
 
 package mz.e.aulas;
 
+import javax.swing.JOptionPane;
+
 
 public class OlaMundo {
     
     public static void main (String[] args) {
         
         System.out.println("Olá Mundo!!");
+        
+        JOptionPane.showMessageDialog(null, "Olá Mundo!!");
         
     }
             
