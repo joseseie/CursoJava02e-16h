1da097bdd432f7a92cc3d784e0414f35ade33c07
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-11 18:46:55 +0200

    List, ArrayList, Vector e Arrays

--- src/mz/e/aula8/JavaList.java
+++ src/mz/e/aula8/JavaList.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mz.e.aula8;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Vector;
 
 /**
  *
  * @author joseseie
  */
 public class JavaList {
+    
+    public static void main(String[] args) {
+        
+         ArrayList<Object> lista = new ArrayList();
+         Vector vector= new Vector(2, 5);
+         
+         Object[] nomes = new Object[10];
+         
+         lista.add("Efraime");
+         lista.add(10);
+         lista.add('W');
+         lista.add(90);
+         
+         vector.add(10);
+         vector.add("sdsfd");
+         vector.add(89);
+         
+         nomes[0] = "Stelio";
+         nomes[7] = "Mario";
+         nomes[3] = 10;
+         nomes[8] = 'K';
+        
+         
+         System.out.println("Array elements: " + Arrays.toString(nomes));
+         System.out.println("Array size: " + nomes.length);
+         
+         System.out.println("Lista elements: " + lista.toString());
+         System.out.println("Lista size: " + lista.size());
+         
+         System.out.println("Vector elements: " + vector.toString());
+         System.out.println("Vector size: " + vector.size());
+         System.out.println("Vector capacity: " + vector.capacity());
+         
+         
+    }
     
 }
